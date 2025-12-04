9d6474a46c94bf8b2abbc4ec45fbb23477cce252
ricky <rickycontact9@gmail.com>
2025-12-04 20:39:12 +0100

    expand menu example

--- examples/menu/Main.purs
+++ examples/menu/Main.purs
@@ -2,6 +2,9 @@ module Main where
 
 import Prelude
 
+import Data.Array as Array
+import Data.Traversable (for)
+import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Ink as Ink
 import React.Basic.Hooks as React
@@ -13,15 +16,40 @@ main = do
 
 mkApp :: React.Component {}
 mkApp = do
+  options <- for [ "Red", "Green", "Blue" ] \color -> do
+    React.component "Color" \{ setChoice } -> React.do
+      { isFocused } <- Ink.useFocus
+      React.useEffect [ isFocused ] do
+        when isFocused $ do
+          setChoice color
+        pure $ pure unit
+
+      pure $ Ink.text
+        [ Ink.bold isFocused
+        , Ink.underline isFocused
+        , Ink.italic isFocused
+        ]
+        color
+
   React.component "App" \_ -> React.do
     { exit } <- Ink.useApp
+    choice /\ setChoice <- React.useState' "Red"
+    finalized /\ setFinalized <- React.useState' false
 
-    Ink.useInput true $ exitInputHandler exit
+    Ink.useInput true $ \input key -> do
+      exitInputHandler exit input key
+      when (Ink.keyIsEnter key) (setFinalized true)
+      pure unit
 
-    pure $ Ink.box
-      [] -- Ink.justifyContent "flex-start" ]
-      [ Ink.text [] "Hello world"
-      ]
+    pure $ do
+      if finalized then (Ink.text [] choice)
+      else
+        ( Ink.box
+            []
+            $ Array.intersperse
+                (Ink.text [ Ink.dimColor true ] " | ")
+                (options <#> \comp -> comp { setChoice })
+        )
 
 exitInputHandler :: Effect Unit -> String -> Ink.Key -> Effect Unit
 exitInputHandler exit input key
